e62d527f021f9de88f9db218762f8585fb6f4400
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2023-03-09 10:55:36 +0330

    Import Font: Vazirmaitn , Add some reset styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './App.css'
+
 
 export default function App(){
   return(
     <h1 className="text-3xl font-bold underline">
-      Hello Tailwind!
+      افزودن فونت وزیرمتن از گوگل فونت
     </h1>
   )
 }
