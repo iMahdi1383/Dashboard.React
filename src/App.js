2652da4bfcf87f77d5a7625a38e37e41489e08d0
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2023-03-11 20:18:34 +0330

    FixText App.js : the word 'calendar' fixed

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ const App = () => {
               {/* Apps */}
               <Route path="/kanban" element="Kanban" />
               <Route path="/editor" element="Editor" />
-              <Route path="/calender" element="Calender" />
+              <Route path="/calendar" element="Calendar" />
               <Route path="/color-picker" element="ColorPicker" />
               {/* Charts */}
               <Route path="/line" element="Line" />
